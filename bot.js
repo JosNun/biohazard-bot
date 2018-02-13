016a6445fe78c2073ce81f10db2a41586298604c
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-02-13 10:41:18 -0500

    convert to es6

--- bot.js
+++ bot.js
@@ -1,39 +1,39 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+let Discord = require('discord.io');
+let logger = require('winston');
+let auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    colorize: true
+  colorize: true,
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+let bot = new Discord.Client({
+  token: auth.token,
+  autorun: true,
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on('ready', function(evt) {
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+bot.on('message', (user, userID, channelID, message, evt) => {
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.substring(0, 1) == '!') {
+    let args = message.substring(1).split(' ');
+    let cmd = args[0];
+
+    args = args.splice(1);
+    switch (cmd) {
+      // !ping
+      case 'ping':
+        bot.sendMessage({
+          to: channelID,
+          message: 'Pong!',
+        });
+        break;
+      // Just add any case commands if you want to..
+    }
+  }
 });
