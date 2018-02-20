8e25ea02281cb55ac4b325cff12b5d4ceddd2766
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-02-20 11:11:33 -0500

    commenting and renaming

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
-const github = require('./github-interface.js');
-const tba = require('./tba-interface.js');
+const github = require('./github.js');
+const tba = require('./tba.js');
 
 // Create an instance of a Discord client
 let bot = new Discord.Client();
@@ -39,26 +39,54 @@ bot.on('message', (message) => {
       case 'help':
       case '?':
         message.channel.send(
-          'Current commands include: `!bb help`, `!bb ping`, and `!bb feature [Your feature request here]`'
+          'Current commands include: `!bb help`, `!bb ping`,' +
+            ' and `!bb feature [Your feature request here]`'
         );
         break;
       case 'tba':
-        if (args[1] == 'team') {
-          tba.getTeamInfo(args[2], (data) => {
-            let teamData = JSON.parse(data);
-            let embed = new Discord.RichEmbed();
-            embed.setTitle('Team Stats');
-            embed.setColor('#4cd626');
+        if (!args[1]) args[1] = '';
+        // If the first argument is a number...
+        if (!isNaN(parseInt(args[1]))) {
+          if (args[2]) {
+            switch (args[2]) {
+              case events:
+                break;
+              default:
+                console.log(`Nothing to do for \`${args[2]}\``);
+            }
+          } else {
+            tba.getTeamInfo(args[1], (data) => {
+              let embed = new Discord.RichEmbed();
+              embed.setTitle('Team Stats');
+              embed.setColor('#4cd626');
 
-            Object.keys(teamData).forEach((key) => {
-              if (!teamData[key]) return;
-              embed.addField(key, teamData[key]);
+              Object.keys(data).forEach((key) => {
+                if (!data[key]) return;
+                embed.addField(key, data[key]);
+              });
+              message.channel.send(embed);
             });
-            let teamName = teamData.nickname;
-            message.channel.send(embed);
-          });
+          }
+        } else {
+          switch (args[1].toLowerCase()) {
+            case 'team':
+              tba.getTeamInfo(args[2], (data) => {
+                let embed = new Discord.RichEmbed();
+                embed.setTitle('Team Stats');
+                embed.setColor('#4cd626');
+
+                Object.keys(data).forEach((key) => {
+                  if (!data[key]) return;
+                  embed.addField(key, data[key]);
+                });
+                message.channel.send(embed);
+              });
+              break;
+            case 'help':
+            default:
+              message.channel.send('Valid arguments are `team` and `help`.');
+          }
         }
-        break;
     }
   }
 });
---
+++ tba.js
@@ -0,0 +1,69 @@
+/**
+ * The Blue Alliance module
+ * @module tba
+ */
+
+const https = require('https');
+const auth = require('./auth.json');
+
+let authHeader = {
+  'X-TBA-Auth-Key': auth.tbaToken,
+};
+
+/**
+ * Callback for fetched data
+ *
+ * @callback fetchedCallback
+ * @param {object} - The fetched data
+ */
+
+/**
+ * Get a teams information from TBA
+ * @param {string|Object} options - team number or options object
+ * @param {string|number} options.number - The number of the team
+ * @param {boolean} [options.simple=true] - Whether to fetch a simple version of the data
+ * @param {fetchedCallback} callback - callback that handles the response
+ * @return {Error}
+ */
+module.exports.getTeamInfo = function(options, callback) {
+  let path = `/team/frc`;
+  if (typeof options === 'string') {
+    path += options;
+  } else if (typeof options === 'object') {
+    let num = options.number || undefined;
+    if (!num) return new Error('Team number is not defined');
+    let simple = options.simple ? '/simple' : '';
+
+    path = `${path}${num}${simple}`;
+    console.log(path);
+  }
+  fetchData(path, callback);
+};
+
+/**
+ * Fetch data from TBA
+ * @param {String} path - where the api request should go. For further info, see https://www.thebluealliance.com/apidocs/v3
+ * @param {fetchedCallback} callback - function to be called on completion
+ */
+function fetchData(path, callback) {
+  let options = {
+    hostname: 'www.thebluealliance.com',
+    path: `/api/v3${path}`,
+    headers: authHeader,
+  };
+  let team = https
+    .get(options, (res) => {
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => {
+        rawData += chunk;
+      });
+
+      res.on('end', () => {
+        callback(JSON.parse(rawData));
+      });
+    })
+    .on('error', (e) => {
+      console.log(`uh oh, something broke: ${e}`);
+    });
+}
